fda31a47fbc65c68fc2ff010a66553f15e503002
youssef <youssefjaouadi@gmail.com>
2018-07-02 00:26:49 +0100

    finish homepage design (slider bugs not fixed)

--- web/frontend/js/homepage.js
+++ web/frontend/js/homepage.js
@@ -1,5 +1,35 @@
+//inculde 'frontend/externres/node_modules/@material/slider/dist/mdc.slider.js';
 $("#nav-search").click(function() {
     $('html, body').animate({
-        scrollTop: $("#test").offset().top
+        scrollTop: $("#resto-search-block").offset().top
     }, 2000);
 });
+
+
+$('#search-loc').click(function () {
+    var height=$('#resto-search-block').height()+$('#load').height()-$('.main_title').height();
+    $('.btns-location').hide();
+    $('#resto-search-block').height(height);
+    $(window).resize();
+    $('#load').show();
+    alert('height +load'+ $('#resto-search-block').height());
+    setTimeout(function () {
+        $("#load").hide();
+        height=$('#resto-search-block').height()+$('#resto-block').height()-$('#load').height()-$('.main_title').height();
+        $('#resto-search-block').height(height);
+        $(window).resize();
+        alert('height +resto lis' +$('#resto-search-block').height());
+        $('#resto-block').show();
+    },3000);
+
+});
+$('#search-adr').click(function () {
+    var dialog = new mdc.dialog.MDCDialog(document.querySelector('#map-dialog'));
+    dialog.show();
+    dialog.listen('MDCDialog:accept', function() {
+        // console.log('accepted');
+       // $('#sucsess-box').css('display','block');
+    })
+});
+const slider = new MDCSlider(document.querySelector('.mdc-slider'));
+slider.listen('MDCSlider:change', () => console.log('Value changed to '+slider.value));
